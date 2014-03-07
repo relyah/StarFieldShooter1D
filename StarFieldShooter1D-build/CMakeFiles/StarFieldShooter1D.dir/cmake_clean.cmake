1d68aa3b29509fabcb35878f17a6fecaa1353cd8
berttreb <relyah@hotmail.com>
2014-03-07 10:27:10 +0200

    Initial Project

---
+++ StarFieldShooter1D-build/CMakeFiles/StarFieldShooter1D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/StarFieldShooter1D.dir/main.cpp.o"
+  "StarFieldShooter1D.pdb"
+  "StarFieldShooter1D"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/StarFieldShooter1D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
