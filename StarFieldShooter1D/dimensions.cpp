36abffb3385ebf9b1eb0e42368b72c9a0223dc5b
berttreb <relyah@hotmail.com>
2014-03-09 23:24:27 +0200

    Moving player.

---
+++ StarFieldShooter1D/dimensions.cpp
@@ -0,0 +1,5 @@
+#include "dimensions.h"
+
+Dimensions::Dimensions()
+{
+}
---
+++ StarFieldShooter1D/dimensions.h
@@ -0,0 +1,10 @@
+#ifndef DIMENSIONS_H
+#define DIMENSIONS_H
+
+class Dimensions
+{
+public:
+    Dimensions();
+};
+
+#endif // DIMENSIONS_H
