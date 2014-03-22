08d479048580ef558e295818ee24e0e21ead02fb
relyah@hotmail.com <relyah@hotmail.com>
2014-03-22 23:11:13 +0200

    Test

--- StarFieldShooter1D/OpenGL/openglmanager.cpp
+++ StarFieldShooter1D/OpenGL/openglmanager.cpp
@@ -78,6 +78,7 @@ void OpenGLManager::BeginScene(float r, float g, float b, float a) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
+//
 void OpenGLManager::EndScene() {
     glfwSwapBuffers(window);
 }
